abc1f098265d01c3ec094d9ffa31a604d3939c6e
Abhiraj <abhirajbhattashali@gmail.com>
2022-06-26 17:05:55 +0530

    Initial Commit

---
+++ Dice/DiceApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiceApp.swift
+//  Dice
+//
+//  Created by Abhiraj on 26/06/22.
+//
+
+import SwiftUI
+
+@main
+struct DiceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
